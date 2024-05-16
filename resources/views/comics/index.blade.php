7d80562ef5434992134d95fc4a261cd167cf06a5
mattemutti <mattemutti@gmail.com>
2024-05-16 15:47:27 +0200

    update modal for the delete data in the index page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -32,20 +32,55 @@
                             <td>{{ $comic->series }}</td>
                             <td>{{ $comic->sale_date }}</td>
                             <td>{{ $comic->type }}</td>
-                            <td><a class="btm btn-dark" href="{{ route('comics.show', $comic) }}"> <i
+                            <td><a class="btn btn-dark" href="{{ route('comics.show', $comic) }}"> <i
                                         class="fa-solid fa-eye"></i></a>
-                                <a class="btm btn-dark" href="{{ route('comics.edit', $comic) }}"> <i
+                                <a class="btn btn-dark" href="{{ route('comics.edit', $comic) }}"> <i
                                         class="fa-solid fa-pen-to-square"></i></a>
 
 
-                                <form action="{{ route('comics.destroy', $comic) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $comic->id }}">
+                                    Delete
+                                </button>
 
-                                    <button type="submit" class="btn btn-danger">
-                                        Delete
-                                    </button>
-                                </form>
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitleId" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitleId-{{ $comic->id }}">
+                                                    Attention! Deleting: {{ $comic->title }}
+                                                </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Attention! You are about to delete this record. The operation is DESTRUCTIVE
+                                                ❌❌❌
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                    Close
+                                                </button>
+                                                <form action="{{ route('comics.destroy', $comic) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+
+                                                    <button type="submit" class="btn btn-danger">
+                                                        Confirm
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
 
 
 
